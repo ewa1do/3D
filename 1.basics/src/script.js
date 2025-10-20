5a2270cdc3f3da66c1f11a3343247f20329f0bbf
Edu <eavera97@gmail.com>
2025-10-19 21:52:47 -0400

    animations

--- 1.basics/src/script.js
+++ 1.basics/src/script.js
@@ -1,47 +1,72 @@
+import "./style.css";
 import * as THREE from "three";
+import gsap from "gsap";
+const canvas = document.querySelector("canvas.webgl");
 
-// We need 4 elements to get started:
-
-// * 1.  A scene that will contain objects
 const scene = new THREE.Scene();
-
-// * 2. Some objects
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-
-// ? MESH: Combination of geometry (shape) and a material (how it looks)
 const mesh = new THREE.Mesh(geometry, material);
 
-// Adding the object to scene
 scene.add(mesh);
 
-// * 3. A camera
-
-/**
- * ? The camera is not visible. It's more like a theoretical point of view.
-   When we will do a render of your scene, it will be from that camera's point of view.
- */
-
 const sizes = {
-  width: 600,
-  height: 600,
+    width: 600,
+    height: 600,
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 4;
+camera.position.z = 3;
 scene.add(camera);
 
-// * 4. A renderer
-
-// ? We will simply ask the renderer to render our scene from the camera's point of view,
-// ? and the result will be drawn into a canvas.
-
-// ! Canvas
-const canvas = document.querySelector("canvas.webgl");
-
-// ! Renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.width);
 
-// * First render
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
+
+//  Adapt to framerate
+//? we need to know how much time it's been since the last tick
+
+// Using the javascript's built in Date
+// let time = Date.now();
+
+// Using THREE.JS Clock
+// const clock = new THREE.Clock();
+
+// Using GSAP
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+
+// ANIMATIONS
+const tick = () => {
+    // const currentTime = Date.now();
+    // const deltaTime = currentTime - time;
+    // time = currentTime;
+
+    // Clock
+    // const elapsedTime = clock.getElapsedTime();
+
+    // Update Objects
+    // mesh.position.x += 0.01;
+    // mesh.position.y += 0.01;
+
+    // mesh.rotation.x += 0.01;
+    // mesh.rotation.y += 0.001;
+
+    // mesh.rotation.y = elapsedTime * Math.PI * 2;
+    // mesh.position.y = elapsedTime;
+    // mesh.position.y = Math.sin(elapsedTime);
+    // mesh.position.x = Math.cos(elapsedTime);
+
+    // The cube is not moving, just the camera
+    // camera.position.y = Math.sin(elapsedTime);
+    // camera.position.x = Math.cos(elapsedTime);
+    // camera.lookAt(mesh.position);
+
+    // Render
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
