ae6d746707d6218a4c4abe55145a2fdd7d98ae76
Edu <eavera97@gmail.com>
2025-10-30 21:53:29 -0400

    debug ui

--- 1.basics/src/script.js
+++ 1.basics/src/script.js
@@ -1,47 +1,142 @@
+import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import gsap from "gsap";
+import GUI from "lil-gui";
 
-// We need 4 elements to get started:
+// DEBUG
+const gui = new GUI({
+    width: 600,
+    title: "Nice debug UI",
+    closeFolders: false,
+});
+// gui.close();
+// gui.hide();
 
-// * 1.  A scene that will contain objects
-const scene = new THREE.Scene();
+window.addEventListener("keypress", (e) => {
+    if (e.key.toLowerCase() === "h") {
+        gui.show(gui._hidden);
+    }
+});
 
-// * 2. Some objects
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-
-// ? MESH: Combination of geometry (shape) and a material (how it looks)
-const mesh = new THREE.Mesh(geometry, material);
-
-// Adding the object to scene
-scene.add(mesh);
-
-// * 3. A camera
+const debugObject = {};
 
 /**
- * ? The camera is not visible. It's more like a theoretical point of view.
-   When we will do a render of your scene, it will be from that camera's point of view.
+ * Base
  */
-
-const sizes = {
-  width: 600,
-  height: 600,
-};
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 4;
-scene.add(camera);
-
-// * 4. A renderer
-
-// ? We will simply ask the renderer to render our scene from the camera's point of view,
-// ? and the result will be drawn into a canvas.
-
-// ! Canvas
+// Canvas
 const canvas = document.querySelector("canvas.webgl");
 
-// ! Renderer
-const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(sizes.width, sizes.width);
+// Scene
+const scene = new THREE.Scene();
 
-// * First render
-renderer.render(scene, camera);
+/**
+ * Object
+ */
+debugObject.color = "#a778d8";
+
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color, wireframe: true });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// folders
+const cubeTweaks = gui.addFolder("Awesome cube");
+
+//range
+cubeTweaks.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+
+// checkbox
+cubeTweaks.add(mesh, "visible");
+cubeTweaks.add(material, "wireframe");
+
+// colors
+cubeTweaks.addColor(debugObject, "color").onChange(() => {
+    // console.log(value.getHexString());
+    material.color.set(debugObject.color);
+});
+
+// Functions
+debugObject.spin = () => {
+    gsap.to(mesh.rotation, { y: mesh.rotation.y + Math.PI * 2 });
+};
+cubeTweaks.add(debugObject, "spin");
+
+debugObject.subdivision = 2;
+
+cubeTweaks
+    .add(debugObject, "subdivision")
+    .min(1)
+    .max(20)
+    .step(1)
+    .onFinishChange(() => {
+        const { subdivision } = debugObject;
+
+        mesh.geometry.dispose();
+        mesh.geometry = new THREE.BoxGeometry(1, 1, 1, subdivision, subdivision, subdivision);
+    });
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+    // Update sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+
+    // Update controls
+    controls.update();
+
+    // Render
+    renderer.render(scene, camera);
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick);
+};
+
+tick();
